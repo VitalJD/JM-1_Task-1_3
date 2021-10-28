e37c96ea1e332c9d3a9bf95d92bc63a8ef0534e1
Vitaly Borisenko <vitalyborisenko@gmail.com>
2021-10-28 19:23:12 +0300

    Made changes to correctly display everything on the site

--- js/base.js
+++ js/base.js
@@ -1,16 +1,28 @@
-function readMore() {
-  let dots = document.getElementById("dots");
-  let more = document.getElementById("more");
-  let btn = document.getElementById("btn");
+// let sectionText = document.getElementsByClassName("section__text");
+// let sectionBtn = document.getElementsByClassName("section__btn");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline-block";
-    btn.innerHTML = "Читать далее";
-    more.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btn.innerHTML = "Скрыть";
-    // btn.style.background = url("../img/icon_rotate-180deg.svg");
-    more.style.display = "block";
-  }
-}
+// for (let i = 0; i < sectionBtn.length; i++) {
+//   sectionBtn[i].addEventListener('click', function() {
+//     this.classList.toggle('active');
+//     let sectionText = this.nextElementSibling;
+//     if (sectionText.style.maxHeight) {
+//       sectionText.style.maxHeight = null;
+//     } else {
+//       sectionText.style.maxHeight = sectionText.scrollHeight + 'px';
+//     }
+//   });
+// }
+
+// let moreText  = document.getElementsByClassName("more__text");
+// function readMore() {
+//   if (dots.style.display === "none") {
+//     .style.display = "inline-block";
+//     btn.innerHTML = "Читать далее";
+//     .style.display = "none";
+//   } else {
+//     dots.style.display = "none";
+//     btn.innerHTML = "Скрыть";
+//     // btn.style.background = url("../img/icon_rotate-180deg.svg");
+//     more.style.display = "block";
+//   }
+// }
